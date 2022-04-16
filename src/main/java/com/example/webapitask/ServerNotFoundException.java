89003a63b3d0a19084f2ece1a3505bd6d16465b7
shanureddy4 <shanureddy78@gmail.com>
2022-04-16 12:10:13 +0530

    ServerNotFoundException removed

--- src/main/java/com/example/webapitask/ServerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.webapitask;
-
-public class ServerNotFoundException extends RuntimeException {
-    public ServerNotFoundException(String id) {
-        super("Could not find server(s) "+id);
-    }
-}
